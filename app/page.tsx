ee700ad65ae6f7606ead3b057514a1318cb81ce6
lcurrit <levi.currit@gmail.com>
2024-10-10 11:00:31 -0700

    Component Updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
-import Topping from "./components/topping";
+"use client";
+
+import Form from "./components/Form";
+import Topping from "./components/Topping";
 
 const DATA = [
   { id: "todo-0", name: "Eat", completed: true },
   { id: "todo-1", name: "Sleep", completed: false },
   { id: "todo-2", name: "Repeat", completed: false },
 ];
+
+function addTopping(name: string): void {
+  alert(name);
+}
 
 export default function Home() {
   const taskList = DATA.map((task) => (
@@ -19,40 +26,7 @@ export default function Home() {
   return (
     <div className="todoapp stack-large">
       <h1>Meatza Pizza</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
-      <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-      </div>
+      <Form addTopping={addTopping} />
       <h2 id="list-heading">3 tasks remaining</h2>
       <ul
         role="list"
